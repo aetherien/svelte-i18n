83ba8dc98e01dba04311b40b206a39275514cae8
ml-aetherien <matthew.lowden@aetherien.com>
2026-05-09 07:58:35 +0200

    Refactor - use export from pattern (sonar code smell)

--- src/runtime/index.ts
+++ src/runtime/index.ts
@@ -1,29 +1,6 @@
-import { getCurrentLocale, $locale } from './stores/locale';
-import { getOptions, init } from './configs';
-import { flush, registerLocaleLoader } from './modules/loaderQueue';
-import {
-  getLocaleFromHostname,
-  getLocaleFromPathname,
-  getLocaleFromNavigator,
-  getLocaleFromQueryString,
-  getLocaleFromHash,
-} from './modules/localeGetters';
-import { $dictionary, $locales, addMessages } from './stores/dictionary';
-import { $isLoading } from './stores/loading';
-import {
-  $format,
-  $formatDate,
-  $formatNumber,
-  $formatTime,
-  $getJSON,
-} from './stores/formatters';
-import {
-  getDateFormatter,
-  getNumberFormatter,
-  getTimeFormatter,
-  getMessageFormatter,
-} from './modules/formatters';
-import { unwrapFunctionStore } from './modules/unwrapFunctionStore';
+import { getCurrentLocale } from './stores/locale';
+import { getOptions } from './configs';
+import { flush } from './modules/loaderQueue';
 
 import type { MessageObject } from './types/index';
 
@@ -36,17 +13,18 @@ export function waitLocale(locale?: string) {
   return flush(locale || getCurrentLocale() || getOptions().initialLocale!);
 }
 
+// setup
+export { init } from './configs';
+export { addMessages } from './stores/dictionary';
+export { registerLocaleLoader as register } from './modules/loaderQueue';
+
+// stores
+export { $locale as locale } from './stores/locale';
+export { $dictionary as dictionary, $locales as locales } from './stores/dictionary';
+export { $isLoading as isLoading } from './stores/loading';
+
+// reactive methods
 export {
-  // setup
-  init,
-  addMessages,
-  registerLocaleLoader as register,
-  // stores
-  $locale as locale,
-  $dictionary as dictionary,
-  $locales as locales,
-  $isLoading as isLoading,
-  // reactive methods
   $format as format,
   $format as _,
   $format as t,
@@ -54,16 +32,22 @@ export {
   $formatNumber as number,
   $formatTime as time,
   $getJSON as json,
-  // low-level
+} from './stores/formatters';
+
+// low-level
+export {
   getDateFormatter,
   getNumberFormatter,
   getTimeFormatter,
   getMessageFormatter,
-  // utils
-  unwrapFunctionStore,
+} from './modules/formatters';
+
+// utils
+export { unwrapFunctionStore } from './modules/unwrapFunctionStore';
+export {
   getLocaleFromHostname,
   getLocaleFromPathname,
   getLocaleFromNavigator,
   getLocaleFromQueryString,
   getLocaleFromHash,
-};
+} from './modules/localeGetters';
